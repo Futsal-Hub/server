d89cfe46dda4977a6964e33d0082ee126ef3662b
efrizal-m <efrizalsy0424@gmail.com>
2021-01-26 00:42:43 +0700

    geocoded-position 1.install axios 2.set env variable 3.fetch result

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -6,7 +6,6 @@ const { ObjectId } = require("mongodb")
 module.exports = async (req,res, next) => {
     try {
         const { access_token } = req.headers
-        console.log(access_token, "<<< access_token auth");
         if (!access_token) {
             console.log("no access token")
             throw { status: 401, message: `You have to login first`}
