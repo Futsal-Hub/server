bbc1d57016ba4d57d5b6a177fe903ee43fda12c8
sangsangga <wandaputrasangga@gmail.com>
2021-01-27 23:31:08 +0700

    add socket io realtime

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const http = require("http")
-const app = require("./app")
-const server = require("http").createServer(app)
-const io = require("socket.io")(server)
-const PORT = 3000
-
+const http = require("http");
+const app = require("./app");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const PORT = 3000;
 
 io.on("connection", (socket) => {
-  console.log("user connected")
+  console.log("user connected");
   socket.on("update", (args) => {
     console.log(args, "<<< args");
-  })
+  });
 
-  socket.on("finish addBooking", () => {
-    io.emit("fetch booking")
-  })
-})
-
+  socket.on("invite sent", () => {
+    io.emit("fetch request");
+  });
+});
 
 server.listen(PORT, () => {
-  console.log("listening")
-})
+  console.log("listening");
+});
